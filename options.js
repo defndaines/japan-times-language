73e2fe9e67163b0a715d3ab7d58471504535aab9
Michael Daines <953037+defndaines@users.noreply.github.com>
2018-12-01 10:13:46 -0800

    Change level options depending upon selected source

--- options.js
+++ options.js
@@ -1,14 +1,36 @@
-let div = document.querySelector('#level-select');
-let frag = document.createDocumentFragment();
-let select = document.createElement('select');
-select.id = 'level';
+const juyoOptions = [1, 2, 3, 4, 5, 6, 9];
+const waniKaniOptions = Array.from({length: 60}, (_, i) => i + 1);
 
-juyoOptions = [1, 2, 3, 4, 5, 6, 9];
-waniKaniOptions = Array.from({length: 60}, (_, i) => i + 1);
+const updateOptions = (value) => {
+  let options = [];
+  if (value === 'Jūyō') {
+    options = juyoOptions;
+  } else if (value === 'WaniKani') {
+    options = waniKaniOptions;
+  }
 
-waniKaniOptions.forEach(n => {
-  select.options.add(new Option(n, n));
+  const div = document.querySelector('#level-select');
+  while (div.hasChildNodes()) {
+    div.removeChild(div.firstChild);
+  }
+  const frag = document.createDocumentFragment();
+  const select = document.createElement('select');
+  select.id = 'level';
+
+  options.forEach(n => {
+    select.options.add(new Option(n, n));
+  });
+
+  frag.appendChild(select);
+  div.appendChild(frag);
+};
+
+const sourceRadio = document.levelSelect.source;
+
+sourceRadio.forEach(radio => {
+  radio.addEventListener('change', () => {
+    updateOptions(radio.value);
+  });
 });
 
-frag.appendChild(select);
-div.appendChild(frag);
+updateOptions(sourceRadio.value);
